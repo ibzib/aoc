7a81be7c0e3e92c9cd0a556d2401d7d256e1503b
Kyle Weaver <kylecweaver@gmail.com>
2024-12-15 18:52:27 -0800

    day 3 pt 1

---
+++ day3-pt1/src/main.rs
@@ -0,0 +1,23 @@
+use num_bigint::BigInt;
+use regex::Regex;
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    let stdin = io::stdin();
+    let lines: Vec<String> = stdin.lock().lines().map(|l| l.unwrap()).collect();
+
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut ans = BigInt::ZERO;
+
+    for s in lines {
+        for c in re.captures_iter(&s) {
+            // dbg!(&c);
+            let x: i32 = c[1].parse().unwrap();
+            let y: i32 = c[2].parse().unwrap();
+            ans += x * y;
+        }
+    }
+
+    println!("{}", ans);
+}
